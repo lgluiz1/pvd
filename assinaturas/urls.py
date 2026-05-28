ae6d107cba0f64c8a3827f4366078c09eb93fe8c
lgluiz1 <legalhints@gmail.com>
2026-05-28 13:27:48 -0300

    fix(assinaturas): resolver conflito de rota mudando financeiro/ para assinatura/

--- assinaturas/urls.py
+++ assinaturas/urls.py
@@ -4,8 +4,8 @@ from assinaturas import views, pix_views
 app_name = 'assinaturas'
 
 urlpatterns = [
-    path('financeiro/', views.portal_financeiro, name='portal_financeiro'),
-    path('financeiro/fatura/<uuid:fatura_id>/', views.detalhe_fatura, name='detalhe_fatura'),
-    path('financeiro/recibo/<uuid:fatura_id>/', views.download_recibo, name='download_recibo'),
-    path('financeiro/pix/<uuid:fatura_id>/', pix_views.gerar_pix_fatura, name='gerar_pix'),
+    path('assinatura/', views.portal_financeiro, name='portal_financeiro'),
+    path('assinatura/fatura/<uuid:fatura_id>/', views.detalhe_fatura, name='detalhe_fatura'),
+    path('assinatura/recibo/<uuid:fatura_id>/', views.download_recibo, name='download_recibo'),
+    path('assinatura/pix/<uuid:fatura_id>/', pix_views.gerar_pix_fatura, name='gerar_pix'),
 ]
